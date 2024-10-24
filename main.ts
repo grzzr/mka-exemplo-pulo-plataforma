afd7e5a5f4cbe784935e046ec32eb0a1a9470edc
grzzr <98114095+grzzr@users.noreply.github.com>
2024-10-24 09:17:27 -0300

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,30 @@
+controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
+    if (jogador.isHittingTile(CollisionDirection.Bottom)) {
+        jogador.vy += -150
+    }
+})
+let jogador: Sprite = null
+tiles.setCurrentTilemap(tilemap`level2`)
+jogador = sprites.create(img`
+    . . . . . . f f f f . . . . . . 
+    . . . . f f f 2 2 f f f . . . . 
+    . . . f f f 2 2 2 2 f f f . . . 
+    . . f f f e e e e e e f f f . . 
+    . . f f e 2 2 2 2 2 2 e e f . . 
+    . . f e 2 f f f f f f 2 e f . . 
+    . . f f f f e e e e f f f f . . 
+    . f f e f b f 4 4 f b f e f f . 
+    . f e e 4 1 f d d f 1 4 e e f . 
+    . . f e e d d d d d d e e f . . 
+    . . . f e e 4 4 4 4 e e f . . . 
+    . . e 4 f 2 2 2 2 2 2 f 4 e . . 
+    . . 4 d f 2 2 2 2 2 2 f d 4 . . 
+    . . 4 4 f 4 4 5 5 4 4 f 4 4 . . 
+    . . . . . f f f f f f . . . . . 
+    . . . . . f f . . f f . . . . . 
+    `, SpriteKind.Player)
+jogador.setStayInScreen(true)
+jogador.ay = 200
+controller.moveSprite(jogador, 100, 0)
+tiles.placeOnRandomTile(jogador, sprites.dungeon.stairEast)
+scene.cameraFollowSprite(jogador)
